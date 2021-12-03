e29021e4aa0ccf792deb1b28a9b5a5bf28e8e7b0
xavierforestier <xavier.forestier@free.fr>
2021-12-03 10:21:27 +0100

    Update forti.js

--- forti.js
+++ forti.js
@@ -1,3 +1,4 @@
+var timeout=0;
 function toggleFav(conn) {
 	const favs = JSON.parse(window.localStorage["fav"] || "[]");
 	if(favs.indexOf(conn) < 0) {
@@ -7,11 +8,23 @@ function toggleFav(conn) {
 	else {
 		window.localStorage.setItem("fav", JSON.stringify(favs.filter(f => f!=conn)));
 	}
+	clearTimeout(timeout);
+	refreshProjet();
+}
+function setDef(conn) {
+	const favs = JSON.parse(window.localStorage["fav"] || "[]");
+	window.localStorage.setItem("auto_connect",conn);
+	if(favs.indexOf(conn) < 0) {
+		favs.push(conn);
+		window.localStorage.setItem("fav", JSON.stringify(favs));
+	}
+	clearTimeout(timeout);
+	refreshProjet();
 }
 function connect(conn) {
 	const ajax=new XMLHttpRequest();
 	ajax.onload = function() {
-		if( (ajax.readyState === xhr.DONE) && (ajax.status === 200) ) {
+		if( (ajax.readyState === ajax.DONE) && (ajax.status === 200) ) {
 			refreshProjet();
 		}
 	};
@@ -23,18 +36,18 @@ function refreshProjet(){
 	const def = window.localStorage["auto_connect"] || "";
 	const ajax=new XMLHttpRequest();
 	ajax.onload = function() {
-		if( (ajax.readyState === xhr.DONE) && (ajax.status === 200) ) {
+		if( (ajax.readyState === ajax.DONE) && (ajax.status === 200) ) {
 			const jsonResp = JSON.parse(ajax.responseText);
 			var html="<table><tr><th>Connexion</th><th>Etat</th><th>Favoris</th></tr>";
 			//Les connexions actives
-			jsonResp.filter(conn=> conn.active).forEach(active => { html += "<tr><td>" + active.name + "</td><td>Connecté</td><td>" + ( active.name==def ?"<button disabled>Défaut</button>":"<button onclick='window.localStorage.setItem(\"auto_connect\",\"" +  active.name + "\")'>Défaut</button>") + "<button onclick='toggleFav(\"" +  active.name + "\")'>" + (favs.indexOf(active.name) < 0 ?"Fav":"Unfav") + "</button></td></tr>" });
+			jsonResp.filter(conn=> conn.active).forEach(active => { html += "<tr class='text-success'><td>" + active.name + "</td><td>Connecté</td><td>" + ( active.name==def ?"<button disabled>Auto-connect</button>":"<button onclick='setDef(\"" +  active.name + "\")'>Défaut</button>") + "<button onclick='toggleFav(\"" +  active.name + "\")'>" + (favs.indexOf(active.name) < 0 ?"☆":"★") + "</button></td></tr>" });
 			//Les connexions favorites
-			jsonResp.filter(conn=> (!conn.active) && (favs.indexOf(conn.name) >= 0 )).forEach(active => { html += "<tr><td>" + active.name + "</td><td><button onclick='connect(\"" + conn.name + "\")'>Se connecter</button></td><td>" + ( active.name==def ?"<button disabled>Défaut</button>":"<button onclick='window.localStorage.setItem(\"auto_connect\",\"" +  active.name + "\")'>Défaut</button>") + "<button onclick='toggleFav(\"" +  active.name + "\")'>Unfav</button></td></tr>" });
+			jsonResp.filter(conn=> (!conn.active) && (favs.indexOf(conn.name) >= 0 )).forEach(active => { html += "<tr><td>" + active.name + "</td><td><button onclick='connect(\"" + active.name + "\")'>Se connecter</button></td><td>" + ( active.name==def ?"<button disabled>Défaut</button>":"<button onclick='setDef(\"" +  active.name + "\")'>Auto-connect</button>") + "<button onclick='toggleFav(\"" +  active.name + "\")'>★</button></td></tr>" });
 			//Le reste
-			jsonResp.filter(conn=> (!conn.active) && (favs.indexOf(conn.name) < 0 )).forEach(active => { html += "<tr><td>" + active.name + "</td><td><button onclick='connect(\"" + conn.name + "\")'>Se connecter</button></td><td>" + ( active.name==def ?"<button disabled>Défaut</button>":"<button onclick='window.localStorage.setItem(\"auto_connect\",\"" +  active.name + "\")'>Défaut</button>") + "<button onclick='toggleFav(\"" +  active.name + "\")'>Fav</button></td></tr>" });
+			jsonResp.filter(conn=> (!conn.active) && (favs.indexOf(conn.name) < 0 )).forEach(active => { html += "<tr><td>" + active.name + "</td><td><button onclick='connect(\"" + active.name + "\")'>Se connecter</button></td><td>" + ( active.name==def ?"<button disabled>Défaut</button>":"<button onclick='setDef(\"" +  active.name + "\")'>Auto-connect</button>") + "<button onclick='toggleFav(\"" +  active.name + "\")'>☆</button></td></tr>" });
 			document.querySelector("div.list-group").innerHTML = html + "</table>";
 		}
-		window.setTimeout(refreshProjet, 30000);
+		timeout=window.setTimeout(refreshProjet, 30000);
 	};
 	ajax.open('GET', document.location.origin + '/api/v2/cmdb/firewall/addrgrp/');
 	ajax.send();
